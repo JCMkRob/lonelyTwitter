0c411c1fd8c52420a932028333913de941c28f85
JCMkRob <jcrobert@ualberta.ca>
2017-02-14 18:25:36 -0700

    My branch

---
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
@@ -0,0 +1,11 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import android.test.ActivityInstrumentationTestCase2;
+/**
+ * Created by joshuarobertson on 2017-02-14.
+ */
+
+public class TweetListTest extends ActivityInstrumentationTestCase2 {
+
+    public TweetListTest
+}
---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetList.java
@@ -0,0 +1,8 @@
+package ca.ualberta.cs.lonelytwitter;
+
+/**
+ * Created by joshuarobertson on 2017-02-14.
+ */
+
+public class TweetList {
+}
